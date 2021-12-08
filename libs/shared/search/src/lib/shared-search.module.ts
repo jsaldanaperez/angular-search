f243a5609ac5abdc5a8083128baf39386c64fe6c
jperez <j.saldanaperez@gmail.com>
2021-12-08 22:50:30 +0100

    clean up

--- libs/shared/search/src/lib/shared-search.module.ts
+++ libs/shared/search/src/lib/shared-search.module.ts
@@ -4,6 +4,7 @@ import { SearchItemComponent } from './search-item/search-item.component';
 import { SearchComponent } from './search/search.component';
 import { SearchModalComponent } from './search-modal/search-modal.component';
 import { FormsModule } from '@angular/forms';
+import { TabIndexService } from './tab-index.service';
 
 @NgModule({
   imports: [CommonModule, FormsModule],
@@ -16,6 +17,7 @@ import { FormsModule } from '@angular/forms';
     SearchItemComponent,
     SearchComponent,
     SearchModalComponent
-  ]
+  ],
+  providers: [TabIndexService]
 })
 export class SharedSearchModule {}
