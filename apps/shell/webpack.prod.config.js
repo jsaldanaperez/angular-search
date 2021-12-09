58df6597126ca95d2999a9db233607873f6599e0
jperez <j.saldanaperez@gmail.com>
2021-12-09 19:19:44 +0100

    fixed webpack

--- apps/shell/webpack.prod.config.js
+++ apps/shell/webpack.prod.config.js
@@ -1,5 +1,6 @@
 module.exports = require('./webpack.config');
 
-module.exports.plugins[0].remotes = {
+module.exports.plugins[0]._options.remotes = {
     "articles": "articles@https://kind-mud-083818c03.azurestaticapps.net/remoteEntry.js",
+    "customers": "customers@https://mango-pebble-0196b3203.azurestaticapps.net/remoteEntry.js"
 }
