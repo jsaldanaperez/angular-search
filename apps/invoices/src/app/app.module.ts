15cbe32f5b7780f7dd92368cedc4e4395a5750a5
jperez <j.saldanaperez@gmail.com>
2021-12-11 11:15:19 +0100

    initial setup list and details

--- apps/invoices/src/app/app.module.ts
+++ apps/invoices/src/app/app.module.ts
@@ -8,11 +8,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { InvoiceListComponent } from './invoice-list/invoice-list.component';
+import { InvoiceDetailsComponent } from './invoice-details/invoice-details.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, InvoiceListComponent, InvoiceDetailsComponent],
   imports: [
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot([ { path: '', loadChildren: () => import('./remote-entry/entry.module').then(m => m.RemoteEntryModule)}], { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
