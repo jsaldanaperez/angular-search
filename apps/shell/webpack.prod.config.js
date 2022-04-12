3bea5732de3682b39112cd995117cf1d0d044d30
José Saldaña Perez <j.saldanaperez@gmail.com>
2022-04-12 21:38:16 +0200

    updated webpack prod

--- apps/shell/webpack.prod.config.js
+++ apps/shell/webpack.prod.config.js
@@ -1,7 +1,7 @@
 module.exports = require('./webpack.config');
 
 module.exports.plugins[0]._options.remotes = {
-    "articles": "articles@https://kind-mud-083818c03.azurestaticapps.net/remoteEntry.js",
-    "customers": "customers@https://mango-pebble-0196b3203.azurestaticapps.net/remoteEntry.js",
-    "invoices": "invoices@https://green-meadow-08ac36903.azurestaticapps.net/remoteEntry.js"
+    "articles": "https://kind-mud-083818c03.azurestaticapps.net/remoteEntry.js",
+    "customers": "https://mango-pebble-0196b3203.azurestaticapps.net/remoteEntry.js",
+    "invoices": "https://green-meadow-08ac36903.azurestaticapps.net/remoteEntry.js"
 }
